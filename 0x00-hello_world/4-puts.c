8d5068142929b418edc526aac68bf96502c8fe4b
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-06-19 19:01:26 +0000

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc -E $CFILE
+i#!/bin/bash
+nclude <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+pust (test("Programming is like building a multilingual puzzle));
+    return (0);
+}
