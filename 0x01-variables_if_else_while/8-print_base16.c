e0cf906b0384fe968d95a687c31acf95cf0ae0dd
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-06-21 00:29:43 +0000

    question 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char gg;
+
+	char lol;
+
+	for (gg = 48; gg <= 57; gg++)
+		putchar(gg);
+	for (lol = 97 ; lol <= 102; lol++)
+		putchar(lol);
+	putchar('\n');
+	return (0);
+}
