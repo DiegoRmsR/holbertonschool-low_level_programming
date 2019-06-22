31927cd2f9909e7b8c8fa67121d9fc1cfa1e126c
Daniel Ramos <drramosdev@gamil.com>
2019-06-21 22:52:06 -0500

    advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int i;
+        int a;
+        int gg = 49;
+
+        for (i = 48; i <= 57; i++)
+        {
+                for (a = gg; a <= 57; a++)
+                {
+                        putchar(i);
+                        putchar(a);
+                        if (i == 56 && i == 57)
+                        {
+                                return(0);
+                        }
+                        if (i != 56 || a != 57)
+                        {
+                                putchar(',');
+                                putchar(' ');
+                        }
+                }
+                gg++;
+        }
+        putchar('\n');
+        return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int a;
+	int e;
+	int d = 0;
+	int gg = 44;
+	int lol = 32;
+
+	for (i = 48; i <= 55; i++)
+	{
+		for (a = (49 + d); a <= 56; a++)
+		{
+			for (e = (a + 1); e <= 57; e++)
+			{
+				putchar(i);
+				putchar(a);
+				putchar(e);
+				if (i == 55 && a == 56 && e == 57)
+				{
+					putchar('\n');
+					return (0);
+				}
+				putchar(gg);
+				putchar(lol);
+			}
+		}
+		d++;
+	}
+	return (0);
+}
