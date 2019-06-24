df9db8985a0512f4ebf38fa555f9cd56e20d2044
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-06-24 23:47:19 +0000

    firts commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+int gg;
+
+	for (gg = 'a'; gg <= 'z'; gg++)
+		_putchar(gg);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+
+{
+int ix10;
+	for(i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n')
+}
