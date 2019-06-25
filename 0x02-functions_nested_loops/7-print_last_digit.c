ab7f69ded0b8b1f4fe640af6cabcf373fc2c2b15
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-06-25 19:48:57 +0000

    question 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - print last digit
+ * @i: variable
+ * Return: print i
+ */
+int print_last_digit(int i)
+{
+int c;
+	if (i >= 0)
+	{
+		c = i % 10;
+	}
+	else
+	{
+		c = -i % 10;
+	}
+	_putchar (c + '0');
+	return (c);
+}
