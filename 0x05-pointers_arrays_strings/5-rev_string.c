df5f52d1a2f9088c024a4ed9876936156af9a020
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-03 01:58:27 +0000

    questions 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * rev_string -  reverses a string.
+ *
+ * @s: string
+ * Return: void.
+ *
+ */
+void rev_string(char *s)
+
+{
+	/* local variable declaration */
+	int i, c, e;
+	char v[400];
+
+	i = 0;
+
+	while (s[i] != ('\0'))
+	{
+	*(v + i) = *(s + i);
+	i++;
+	}
+
+	e = i - 1;
+	for (c = 0 ; c < i ; c++)
+	{
+	*(s + c) = *(v + e);
+	e--;
+	}
+
+}
