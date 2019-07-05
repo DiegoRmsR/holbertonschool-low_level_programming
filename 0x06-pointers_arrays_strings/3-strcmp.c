0f9a57c08fc789710f141981c68c546edb9c50d6
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-05 20:54:58 +0000

    question 2 and 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: string
+ * @s2: string
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while(s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (s2[i] - s1[i]);
+}
