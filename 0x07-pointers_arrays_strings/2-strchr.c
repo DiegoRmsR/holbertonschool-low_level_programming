9c525b91cf4feb688cea81705d62555e348c21d6
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-08 19:19:28 +0000

    question 0,1,2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strchr - function that locates a character in a string
+ * @s: pointer string
+ * @c: character
+ * Return: pointer to the first occurrence of the character c in the string s
+*/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		else if (s[i] == 0)
+		{
+			return (0);
+		}
+	}
+	return(0);
+}
