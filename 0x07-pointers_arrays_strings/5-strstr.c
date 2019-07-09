486dd30e80b77886bc3497f96c8ecc07aa658367
DiegoRmsR <diegoramos.mt@gmail.com>
2019-07-08 22:50:28 -0500

    correction 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,22 +1,19 @@
 #include "holberton.h"
-
 /**
  * _strstr -  Locates a substring.
- *
  * @haystack: String
  * @needle: Substring
  * Return: pointer to the beginning of the located substring,
- *         or NULL if the substring is not found.
- *
+ * or NULL if the substring is not found.g
  */
 char *_strstr(char *haystack, char *needle)
 {
 	/* local variable declaration */
-	unsigned int i, j, k, l;
+	unsigned int i, j, c, l;
 
-	k = 0;
-	while (*(needle + k) != '\0')
-		k++;
+	c = 0;
+	while (*(needle + c) != '\0')
+		c++;
 
 	l = 0;
 	while (*(haystack + l) != '\0')
@@ -24,11 +21,11 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0 ; i < l ; i++)
 	{
-		for (j = 0 ; j < k ; j++)
+		for (j = 0 ; j < c ; j++)
 		{
 			if (*(needle + j) != *(haystack + i + j))
 				break;
-			if (j == k - 1)
+			if (j == c - 1)
 				return (haystack + i);
 			if (i + j == l - 1)
 				return (0);
