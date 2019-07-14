e59705cb92615598990e9d0ce05ffc5e12577172
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-13 21:17:45 -0500

    Fix: Betty

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,9 +3,10 @@
  * main - prints the number of arguments passed into it
  * @argc: number of arguments
  * @argv: array thet contains arguments of argc
+ * Return: 0
 */
-int main(int argc,__attribute__((unused))char *argv[])
+int main(int argc, __attribute__((unused))char *argv[])
 {
-	printf ("%d\n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
