083bd72bcde18b4ef8b649cbe34164c46f0e5020
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-16 23:45:51 -0500

    Add: files

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * argstostr - Concatenates all the arguments of your program.
+ *
+ * @ac: Argument count
+ * @av: Argument values
+ *
+ * Return: string.
+ */
+char *argstostr(int ac, char **av)
