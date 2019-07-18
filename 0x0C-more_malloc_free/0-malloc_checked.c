c7b8f2e7db3a6cbf0071cc3c2510462ce671cc35
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-18 18:06:28 +0000

    Fix: Rename 0x0C

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory.
+ * @b: variable bytes
+ * Return: pointer to the allocated memory
+*/
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *i;
+
+	i = malloc(sizeof(unsigned int) * b);
+	{
+		if (!i)
+		{
+			exit(98);
+		}
+	}
+	return (i);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: pointer string char
+ * @s2: pointer string char
+ * @n: variable
+*/ 
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i;
+	int j;
+	unsigned int *p
+
+	
+
+}
