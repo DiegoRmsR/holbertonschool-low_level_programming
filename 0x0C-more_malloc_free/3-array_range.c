1205b589f27a9aefe90bf612d171b00f418532d7
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-19 00:21:57 +0000

    Fix: line 24, recorre malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,7 +9,7 @@
 int *array_range(int min, int max)
 {
 	int *p;
-	int i = 0;
+	int i;
 
 	if (min > max)
 	{
@@ -21,7 +21,7 @@ int *array_range(int min, int max)
 	{
 		return (NULL);
 	}
-	for (i = 0; p[i] < max; i++)
+	for (i = 0; i < max - min + 1 ; i++)
 	{
 		p[i] = min + i;
 	}
