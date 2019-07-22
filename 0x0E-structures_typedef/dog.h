6f8b7b66ed1ec5c2185e8e42395e91836a121646
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-22 22:43:34 +0000

    Add: Struct with elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Object dog
+ * @age: numbers float
+ * @name: pointer char name
+ * @owner: pointer char
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
