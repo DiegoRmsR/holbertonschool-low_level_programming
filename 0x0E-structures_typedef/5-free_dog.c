218d6cac3a0bd0343d75fa46b1ec23940da19994
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-23 02:16:11 +0000

    Add: Function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Call free function
+ * @d: Receives the argument type struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
