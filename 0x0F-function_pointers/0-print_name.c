564e90279d63df240005ba8976bed91939d4d514
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-24 15:29:47 +0000

    Update: Add return and f with name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,16 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+
 /**
  * print_name - pointer of name
- * @name: pointer of name printf
+ * @name: pointer of name print take name
  * @f: pointer of funcion
+ *
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	if (name == '\0' || f == '\0')
+		return;
+	(*f)(name);
 }
