ef2d9a6ae543ab420f6752212ba1471bfb027eda
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-24 20:07:25 +0000

    Fix: inclide library stdlib

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdlib.h>
+
 /**
  * int_index - function that searches for an integer.
  * @array: pointer array of numbers
@@ -11,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array != '\0' && cmp != '\0')
+	if (array != NULL && cmp != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
