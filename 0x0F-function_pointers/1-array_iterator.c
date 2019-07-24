a3b24a68a6c7233ad6cacfced5e4c140a6fd6139
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-24 17:55:26 +0000

    Fix: Add condition if (== NULL)

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,8 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
-	for (i = 0; i < size; i++)
-	{
-		(*action)(array[i]);
-	}
+	if (action != NULL && array != NULL)
+		for (i = 0; i < size; i++)
+			(*action)(array[i]);
 }
