253d10889b336723348fc31d4c5a06582d53118a
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-25 23:36:21 +0000

    Update: New prototipe if

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,6 +17,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
 	operation = get_op_func(argv[2]);
 
