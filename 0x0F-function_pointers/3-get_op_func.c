0432ff0ce302078228d5b26fbf2c3f5a15ebb752
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-25 00:40:21 +0000

    Add: files

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,25 @@
+#include <stdlib.ht>
+/**
+ *
+ *
+ *
+*/
+int (*get_op_func(char *s))(int, int)
+struct op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+int i = 0;
+
+while (i < 10)
+{
+	if (s[0] == opsop[i])
+	{
+		return ()
+	}
+	i++
+}
