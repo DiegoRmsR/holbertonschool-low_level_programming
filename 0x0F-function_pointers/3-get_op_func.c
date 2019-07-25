493665a9395ef1d4c3baea383c22715e164a4cd5
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-24 23:31:53 -0500

    Fix: Selects the correct function to perform

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,25 +1,30 @@
-#include <stdlib.ht>
+#include "3-calc.h"
 /**
+ * get_op_func - check if the absolute value of a number is 98
+ * @s: the integer to check
  *
- *
- *
-*/
+ * Return: 0 if false, something else oth
+ */
 int (*get_op_func(char *s))(int, int)
-struct op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
-	};
-int i = 0;
-
-while (i < 10)
 {
-	if (s[0] == opsop[i])
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (i < 5)
 	{
-		return ()
+		if (ops[i].op[0] == *s)
+		{
+			return (ops[i].f);
+		}
+		i++
 	}
-	i++
+	return (NULL);
 }
