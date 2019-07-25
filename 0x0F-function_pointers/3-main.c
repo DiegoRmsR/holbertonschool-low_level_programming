e7e8bc1da125a5447fa03b48b373975e23d4b536
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-25 23:01:28 +0000

    Update: Funtions

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
 
 /**
@@ -12,22 +10,25 @@
 int main(int argc, char *argv[])
 {
 	int a, b;
-	int (*operation)(int, int)
+	int (*operation)(int, int);
 
-	if (argc != 4);
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	operator = get_op_func(argv[2])
 
-	if (operator == NULL)
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
 	{
-			printf("Error\n");
-			exit(99);
+		printf("Error\n");
+		exit(99);
 	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
+
 	printf("%d\n", operation(a, b));
 	return (0);
 }
