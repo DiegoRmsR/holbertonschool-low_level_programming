290e7bd873a284b6e68ab5c289bfb02d2da0a4ef
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-24 23:57:49 -0500

    Fix, main.c and errors betty

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,41 +1,67 @@
 #include <stdlib.h>
-#include "function_pointers.h"
+
+
 /**
  * op_add - sum operation
  * @a: num1 of shell
  * @b: num2 of shell
 ¨* Return: Sum
 */
+
 int op_add(int a, int b)
+{
 	return (a + b);
+}
 /**
  * op_sub - subtraction operation
  * @a: num1 of shell
  * @b: num2 of shell
  * Return: sub
 */
-int op_sub(int a, int b);
+
+int op_sub(int a, int b)
+{
 	return (a - b)
+}
 /**
  * op_mul - multiplication operation
  * @a: num1 of shell
  * @b: num2 of shell
  * Return: Multiplication
 */
-int op_mul(int a, int b);
+
+int op_mul(int a, int b)
+{
 	return (a * b)
+}
 /**
  * op_div - division operation
  * @a: num1 of shell
  * @b: num2 of shell
  * Return:
 */
+
 int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
+}
+
 /**
  * op_mod - operation module
  * @a: num1 of shell
  * @b: num2 of shell
 */
 int op_mod(int a, int b)
-	return (a % b);
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
