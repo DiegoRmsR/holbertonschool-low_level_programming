c0530bdcfaff571241a2932f28ea3d9f86c104f8
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-26 19:24:45 +0000

    Fix: delete else and change conditions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,15 +17,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 		{
 			num = va_arg(list, unsigned int);
-			if (i < n - 1)
+			printf("%d", num);
+			if (i < n - 1 && separator)
 			{
-				printf("%d%s", num, separator);
-			}
-			else
-			{
-				printf("%d\n", num);
+				printf("%s", separator);
 			}
 		}
+		printf("\n");
 		va_end(list);
 	}
 }
