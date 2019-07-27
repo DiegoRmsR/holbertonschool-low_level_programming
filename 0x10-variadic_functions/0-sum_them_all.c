e5b365808dee762a0daa7e5d5a602f64476caedf
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-27 20:24:28 +0000

    Fix: add condition if

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,24 +1,23 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+
 /**
  * sum_them_all - returns the sum of all its parameters
  * @n: size of arguments
+ *
  * Return: Sum
 */
 int sum_them_all(const unsigned int n, ...)
 {
+	int sum = 0;
+	unsigned int i;
 	va_list valist;
-	unsigned int sum = 0;
-	unsigned int i = 0;
 
 	if (n == 0)
-		return (n);
-
+		return (0);
 	va_start(valist, n);
-
 	for (i = 0; i < n; i++)
 		sum += va_arg(valist, int);
 	va_end(valist);
-
 	return (sum);
 }
