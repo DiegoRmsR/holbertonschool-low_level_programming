c42bbfbff7875ddf4f4a65dae455889c84dddedb
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-27 00:35:39 +0000

    Add: Files

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+/**
+ * print_all - function that prints anything
+ * @format: Pointer to strings
+*/
+void print_all(const char * const format, ...);
+{
+	va_list list;
+	int i = 0;
+	char *p;
+
+	va_start(list, format)
+	while (format[i])
+	{
+		switch format[i]
+		{
+			
+		}
+	i++
+	}
+}
