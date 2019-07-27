b4812c21564cf1c80f5a7c17e061c5ed3b3a63e9
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-27 20:10:47 +0000

    Delete: if n == 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,6 @@
  * sum_them_all - returns the sum of all its parameters
  * @n: size of arguments
  * Return: Sum
- *
 */
 int sum_them_all(const unsigned int n, ...)
 {
@@ -11,9 +10,8 @@ int sum_them_all(const unsigned int n, ...)
 	va_list valist;
 	unsigned int i;
 
-	if (n == 0)
-		return (0);
 	va_start(valist, n);
+
 	for (i = 0; i < n; i++)
 		sum += va_arg(valist, int);
 	va_end(valist);
