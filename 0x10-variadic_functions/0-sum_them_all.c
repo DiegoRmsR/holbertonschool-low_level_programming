a0bfb8ec2d491a8d565c83e77fa1573acabf19f9
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-27 20:14:05 +0000

    Fix: orden del codigo

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,14 +6,17 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int sum = 0;
 	va_list valist;
+	unsigned int sum = 0;
 	unsigned int i;
 
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
+	{
 		sum += va_arg(valist, int);
+	}
 	va_end(valist);
+
 	return (sum);
 }
