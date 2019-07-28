b0fb4eef098bec92b2555105f22c35a166969f96
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-07-28 19:19:15 +0000

    Update: New prototipes for task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,4 +9,21 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct op - struct operators
+ * @op: operator
+ * @f: function from print
+ *
+ */
+typedef struct op
+{
+	char *op;
+	void (*f)(va_list);
+} op_t;
+
+void print_number(va_list);
+void print_string(va_list);
+void print_float(va_list);
+void print_char(va_list);
+
 #endif  
