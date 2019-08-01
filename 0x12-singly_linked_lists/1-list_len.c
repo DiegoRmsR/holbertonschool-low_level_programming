3d1acf0dd94731cdebca244db552df0526b7f6c3
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-08-01 23:33:53 +0000

    Add: Count = 0

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 /**
  * list_len - function that returns the number of elements in a linked
- * @h: pointer to list
+ * @h: pointer head
  *
  * Return: number of nodes
 */
 size_t list_len(const list_t *h)
 {
-	size_t nuelem;
+	size_t nuelem = 0;
 
 	while (h != NULL)
 	{
