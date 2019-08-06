bbfd858697224b107d62b687089e75b968192be3
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-08-06 21:56:39 +0000

    Fix: orden

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -17,9 +17,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	tmp = head;
 	for (i = 0; i < index; i++)
 	{
+		tmp = tmp->next;
 		if (head == NULL)
 			return (0);
-		tmp = tmp->next;
 	}
 	return (tmp);
 }
