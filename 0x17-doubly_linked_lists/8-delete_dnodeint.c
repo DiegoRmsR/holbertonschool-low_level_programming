fb913ac166fcd047e26a18afca9b1f61e1f8d0ea
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-09-16 22:54:52 +0000

    Add: condition tmp->next == NULL

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -23,6 +23,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	while (tmp != NULL)
 	{
+		if (tmp->next == NULL)
+			return (-1);
 		if (i == index)
 		{
 			tmp->prev->next = tmp->next;
