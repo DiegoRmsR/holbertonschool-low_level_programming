eb3b2e701b6e436bcdb109dba78a409c396101c6
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-10-29 19:44:48 +0000

    Add: function that sorts an array of integers in ascending order using the Bubble sort algorithm

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ * bubble_sort - function that sorts an array of integers
+ * in ascending order using the Bubble sort algorithm
+ * @array: pointer to elements
+ * @size: size of array
+ *
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	int tmp;
+	int flag;
+
+	if (array == NULL || size < 2)
+		return;
+	while (flag != -1)
+	{
+		flag = -1;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = tmp;
+				print_array(array, size);
+				flag = 1;
+			}
+		}
+	}
+}
