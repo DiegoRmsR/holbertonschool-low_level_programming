f6c940ce7778f781373004636312d4c96b7a14fe
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-10-29 20:10:42 +0000

    Feature: ++i for ++i

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -19,7 +19,7 @@ void bubble_sort(int *array, size_t size)
 	while (flag != -1)
 	{
 		flag = -1;
-		for (i = 0; i < size - 1; i++)
+		for (i = 0; i < size - 1; ++i)
 		{
 			if (array[i] > array[i + 1])
 			{
