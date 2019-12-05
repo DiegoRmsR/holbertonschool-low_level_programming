a8d8b05c84e93d2a5573f0d293466d24efec3aa1
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-12-05 22:34:28 +0000

    [Fetaure] betty

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_node - function that creates a binary tree node
  * @parent: pointer to the parent node
@@ -9,6 +10,9 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
+
+	if (!parent)
+		return (NULL);
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
