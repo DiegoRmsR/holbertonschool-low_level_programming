6d4531a6647f18dca14a09b9ac0f78fb7742fccf
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-12-05 22:41:10 +0000

    [Fetaure] betty

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -1,12 +1,14 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_insert_left - function that inserts a node as the
  * left-child of another node
+ *
  * @parent: pointer to the node
  * @value: value to store in the new node
  *
  * Return: pointer to the created node, or NULL
-*/
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
