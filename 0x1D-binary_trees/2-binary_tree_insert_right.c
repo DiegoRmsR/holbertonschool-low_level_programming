33d38f74e66875ac0bc728473bab324f33128a06
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-12-07 01:19:20 +0000

    [Feature] betty

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -14,6 +14,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 	new_node = binary_tree_node(parent, value);
+	if (!new_node)
+		return (NULL);
 
 	new_node->right = parent->right;
 	parent->right = new_node;
