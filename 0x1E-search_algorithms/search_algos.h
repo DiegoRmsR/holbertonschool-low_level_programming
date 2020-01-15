c21387cf4bcaa3a3d1ccfff44dd8e35ccaa6a925
DiegoRmsR <Diegoramos.mt@gmail.com>
2020-01-15 23:11:45 +0000

    [Feature] library

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SHEARCH_ALGOS_H
+#define SHEARCH_ALGOS_H
+
+/**Libraries*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/**Prototypes*/
+int linear_search(int *array, size_t size, int value);
+#endif
